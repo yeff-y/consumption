f76318688ae08ce7f7f29db9d77c310e0f87b268
叶飞飞(10029669) <yeff@glodon.com>
2020-10-22 16:12:55 +0800

    interceptor

--- src/main/java/com/yeff/consumption/interceptor/ConsumptionInterceptor.java
+++ src/main/java/com/yeff/consumption/interceptor/ConsumptionInterceptor.java
@@ -15,6 +15,6 @@ public class ConsumptionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
         System.out.println("controller 之前执行");
-        return true;
+        return false;
     }
 }
