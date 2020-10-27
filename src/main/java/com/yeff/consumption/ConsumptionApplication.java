d8c2cd3ee260df241bfd213b41b4251e2c3d6713
叶飞飞(10029669) <yeff@glodon.com>
2020-10-27 13:44:28 +0800

    index and home html

--- src/main/java/com/yeff/consumption/ConsumptionApplication.java
+++ src/main/java/com/yeff/consumption/ConsumptionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 
 @MapperScan("com.yeff.consumption.mappers")
 public class ConsumptionApplication {
