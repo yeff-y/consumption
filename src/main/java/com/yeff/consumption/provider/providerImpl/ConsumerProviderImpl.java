95085d877824fcc6500cc6a9d2816d8dea2da2ac
叶飞飞(10029669) <yeff@glodon.com>
2020-10-29 18:43:18 +0800

    general example

--- src/main/java/com/yeff/consumption/provider/providerImpl/ConsumerProviderImpl.java
+++ src/main/java/com/yeff/consumption/provider/providerImpl/ConsumerProviderImpl.java
@@ -3,6 +3,7 @@ package com.yeff.consumption.provider.providerImpl;
 import com.yeff.consumption.dto.ConsumerDto;
 import com.yeff.consumption.mappers.ConsumerMapper;
 import com.yeff.consumption.model.Consumer;
+import com.yeff.consumption.model.ConsumerExample;
 import com.yeff.consumption.provider.ConsumerProvider;
 import com.yeff.consumption.service.impl.ConsumerServiceImpl;
 import org.slf4j.Logger;
@@ -30,7 +31,15 @@ public class ConsumerProviderImpl implements ConsumerProvider {
 
     @Override
     public List<Consumer> selectRecords(String name) {
-        List<Consumer> consumerList = consumerMapper.selectByName(name);
+        ConsumerExample example = _buildExample(name);
+        List<Consumer> consumerList = consumerMapper.selectByExample(example);
         return consumerList;
+    }
+
+    private ConsumerExample _buildExample(String name) {
+        ConsumerExample example = new ConsumerExample();
+        ConsumerExample.Criteria criteria = example.createCriteria();
+        criteria.andConsumerNameEqualTo(name);
+        return example;
     }
 }
